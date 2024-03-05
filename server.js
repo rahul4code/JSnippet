76629f6f81b6f26be621296796e09c5e52344133
rahul4code <you@example.com>
2024-03-05 12:45:42 +0530

    css practice branch

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-  // res.setHeader("Set-Cookie", "name=RahulGupta");
 });
 
 app.listen(PORT, () => {
