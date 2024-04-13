418796233a1d1aaa78ad5c7322a39f8780d73f99
rahul4code <you@example.com>
2024-04-13 13:42:40 +0530

    importing audio and video in HTML 5

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/audio-video")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
