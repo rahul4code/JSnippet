e878d553c687752fd18fdd35669106edd0027034
rahul4code <you@example.com>
2024-06-03 12:01:46 +0530

    block-inline element

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require("path");
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/block-inline")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public/block-inline", "index.html"));
 });
 
 app.listen(PORT, () => {
