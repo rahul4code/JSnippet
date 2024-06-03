6562f1f6f20806b4f749d94696187247990117b8
rahul4code <you@example.com>
2024-06-03 12:52:26 +0530

    applied ways to center div

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require("path");
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public/block-inline")));
+app.use(express.static(path.join(__dirname, "public/center-div")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/block-inline", "index.html"));
+  res.sendFile(path.join(__dirname, "public/center-div", "index.html"));
 });
 
 app.listen(PORT, () => {
