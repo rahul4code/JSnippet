0684fc3edd0fb7c0f33970a481d23a365f1dd35a
rahul4code <you@example.com>
2024-06-11 06:23:17 +0530

    review

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const PORT = 3000;
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const PORT = 3000;
 
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+//   // res.setHeader("Set-Cookie", "name=RahulGupta");
+// });
+
+// app.listen(PORT, () => {
+//   console.log("App is running on port " + PORT);
+// });
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+// New app using express module
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Create your post and get request here to perform addition
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-  // res.setHeader("Set-Cookie", "name=RahulGupta");
 });
 
-app.listen(PORT, () => {
-  console.log("App is running on port " + PORT);
+app.post("/calculate", (req, res) => {
+  // const {a,b, operand}
+});
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
 });
