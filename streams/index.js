d34a3f7e5a2be8312762cbd2a5641f8a39690304
rahul4code <you@example.com>
2024-06-26 22:13:10 +0530

    implemented streams

--- streams/index.js
+++ streams/index.js
@@ -1,5 +1,32 @@
 const fs = require("fs");
+const { Transform } = require("stream");
 
-const writeFile = fs.createWriteStream("./streams/example.txt", "utf-8");
+const readStream = fs.createReadStream("./streams/Readme.txt", "utf-8");
+const writeStream = fs.createWriteStream("./streams/writeme.txt", {
+  flags: "a", // This flag is used to append the data
+});
 
-console.log(writeFile, "writeFile");
+readStream.on("data", (chunk) => {
+  writeStream.write(chunk + "\n");
+  writeStream.write("This is Rahul Gupta!\n");
+  //   writeStream.end();
+});
+
+readStream.on("end", () => {
+  console.log("File is read completely");
+});
+
+writeStream.on("finish", () => {
+  console.log("write is completed");
+});
+
+// convert all data to uppercase
+const transformStream = new Transform({
+  transform(chunk, encoding, callback) {
+    const transformedChunk = chunk.toString().toUpperCase();
+    this.push(transformedChunk);
+    callback();
+  },
+});
+
+readStream.pipe(transformStream).pipe(writeStream);
